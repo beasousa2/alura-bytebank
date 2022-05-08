e97fb539b6f166660e7dba4a8811efd694c35853
Beatriz Sousa <beasousa20@outlook.com>
2022-05-07 21:12:39 -0300

    aula 02

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -3,9 +3,12 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls:['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss']
 })
 
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
 
+  transferir() {
+    console.log('Solicitada nova transferencia');
+  }
 }
